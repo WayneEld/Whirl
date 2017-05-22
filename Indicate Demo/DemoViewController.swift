cadee82c79e0dcb8de95b9df0d47c2cb7c3c6881
WayneEld <WayneEldridge@waynes-macbook-pro.local>
2017-05-22 15:44:27 +0200

    Demo in project and image assets removed.

---
+++ Indicate Demo/DemoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DemoViewController.swift
+//  Indicate
+//
+//  Created by Wayne Eldridge on 2017/05/22.
+//  Copyright © 2017 Wayne Eldridge. All rights reserved.
+//
+
+import UIKit
+
+
+class DemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let indicator = Indicate(indicatorType: .normal)
+        indicator.startIndicator()
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
