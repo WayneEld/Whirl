965d6b14023ff9a3752aace45706935db0b4fbc1
WayneEld <WayneEldridge@waynes-macbook-pro.local>
2017-05-22 15:47:01 +0200

    Demo comments

--- Indicate Demo/DemoViewController.swift
+++ Indicate Demo/DemoViewController.swift
@@ -14,7 +14,6 @@ class DemoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         
         let indicator = Indicate(indicatorType: .normal)
         indicator.startIndicator()
